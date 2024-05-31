83c45c34a8d76d3d68b328830fd18c75e0071074
Mwenda <kelvinfotech@gmail.com>
2024-05-31 13:31:04 +0300

    Python P3 Oo Counting Sentences Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=None):
+        self.value = value
+        if not isinstance(value, str):
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.value is None:
+            return False
+        return self.value.endswith('.')
+
+    def is_question(self):
+        if self.value is None:
+            return False
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        if self.value is None:
+            return False
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        if self.value is None:
+            return 0
+        sentences = self.value.split('.')
+        sentences = [s for s in sentences if s.strip() != '']
+        return len(sentences)
